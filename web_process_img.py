964ad0f5ed0bd510bd2811a4f6ff15ce7ddeef72
Arty_Solomko <artysolomko@gmail.com>
2021-12-28 22:50:50 +0300

    web_app

--- web_process_img.py
+++ web_process_img.py
@@ -1,72 +1,68 @@
-
 import requests
 import os
 import io
 from tqdm import tqdm
 from PIL import Image
-from gooey import Gooey, GooeyParser
-from detection.capture_imgs import CaptureImgs
-from detection.capture_img import CaptureImg
+from detection.capture_web import CaptureWeb
 from detection.predict import Predict
-from detection.save_img import SaveImg
+from detection.save_web import SaveWeb
 from detection.annotate_img import AnnotateImg
 from detection.separate_background import Separate
 from detection.utils import detectron
 from detectron2.data.datasets import register_coco_instances
 from detectron2.data import MetadataCatalog
 from detectron2.data import DatasetCatalog
-from flask import Flask, render_template, request, send_from_directory, send_file
+from flask import Flask, render_template, request, send_file
 
 
-
-web_process_img = Flask(__name__)
-detector = Detector()
-def load_image(url):
-	response = requests.get(url)
-	img = Image.open(io.BytesIO(response.content))
-	return img
+app = Flask(__name__)
 
 
-def run_inference(img_path = 'file.jpg'):
-	result_img = detector.inference(img_path)
-	try:
-		os.remove(img_path)
-	except:
-		pass
-
-	return result_img
+@app.route("/")
+def home():
+    return render_template('home.html')
 
 
-@web_process_img.route("/")
-def index():
-	return render_template('index.html')
+@app.route("/home", methods=['POST', 'GET'])
+def main():
+
+    method = request.method
+    values = [x for x in request.form.values()]
+    confidence_threshold = values[0]
+    mode = values[1]
+
+    if method == 'GET':
+        src = request.args.get("url")
+    elif method == 'POST':
+        src = Image.open(request.files['file'].stream)
+
+    capture_images = CaptureWeb(src)
+
+    cfg = detectron.setup_cfg(config_file="./configs/COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml",
+                              weights_file=None,
+                              confidence_threshold=confidence_threshold)
+    predict = Predict(cfg)
+
+    if mode == "separate_background":
+        separate = Separate("vis_image")
+        annotate_image = None
+    else:
+        separate = None
+        metadata_name = cfg.DATASETS.TEST[0] if len(cfg.DATASETS.TEST) else "__unused"
+        metadata_name = MetadataCatalog.get(metadata_name)
+        annotate_image = AnnotateImg("vis_image", metadata_name)
+
+    save_image = SaveWeb("vis_image")
+
+    pipeline = (capture_images |
+              predict |
+              separate |
+              annotate_image |
+              save_image)
 
 
-@web_process_img.route("/detect", methods=['POST', 'GET'])
-def upload():
-	if request.method == 'POST':
-		try:
-			file = Image.open(request.files['file'].stream)
-			rgb_im = file.convert('RGB')
-			rgb_im.save('file.jpg')
-		except:
-			return render_template("failure.html")
-	elif request.method == 'GET':
-		url = request.args.get("url")
-		try:
-			rgb_im = load_image_url(url)
-			rgb_im = rgb_im.convert('RGB')
-			rgb_im.save('file.jpg')
-		except:
-			return render_template("failure.html")
-	result_img = run_inference('file.jpg')
-	file_object = io.BytesIO()
-	result_img.save(file_object, 'PNG')
-	file_object.seek(0)
-
-	return send_file(file_object, mimetype='image/jpeg')
 
 if __name__ == "__main__":
 
-	port = int(os.environ.get('PORT', 8080))
-	app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port, debug=True)
