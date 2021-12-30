96a5b499c4ab01ce6edeeb8ddc92a94e305b193f
Arty_Solomko <artysolomko@gmail.com>
2021-12-30 22:46:42 +0300

    new_func

--- app.py
+++ app.py
@@ -40,89 +40,101 @@ def home():
 
 @app.route("/predict", methods=['POST', 'GET'])
 def main():
+    try:
+        app.logger.info('in upload route')
+        method = request.method
+        if method == 'GET':
+            url = request.args.get("url")
+            response = requests.get(url)
+            image = Image.open(io.BytesIO(response.content))
+            image.save("image_001.jpeg")
+            src = "image_001.jpeg"
+            mode = "instance_segmentation"
+            CONFIDENCE = .7
+        elif method == 'POST':
+            try:
+                file = request.files['file']
+                if file and allowed_file(file.filename):
+                    upload_result = upload(file,
+                                           public_id="image_001.jpeg")
+                    dump_response(upload_result)
+                    url, options = (cloudinary_url(
+                        upload_result['public_id'],
+                        format=upload_result['format'],
+                        width=200,
+                        height=150,
+                        crop="fill"
+                    )
+                    )
+                    url = upload_result['secure_url']
+                    response = requests.get(url)
+                    image = Image.open(io.BytesIO(response.content))
+                    image.save("image_001.jpeg")
+                    src = "image_001.jpeg"
+                    mode = request.form["mode"]
+                    CONFIDENCE = float(request.form["confidence"])
+            except:
+                return render_template("error.html")
 
-    CONFIDENCE = .7
-    app.logger.info('in upload route')
-    method = request.method
-    if method == 'GET':
-        url = request.args.get("url")
-        response = requests.get(url)
-        image = Image.open(io.BytesIO(response.content))
-        image.save("image_001.jpeg")
-        src = "image_001.jpeg"
-        mode = "instance_segmentation"
-    elif method == 'POST':
-        try:
-            file = request.files['file']
-            if file and allowed_file(file.filename):
-                upload_result = upload(file,
-                                       public_id="image_001.jpeg")
-                dump_response(upload_result)
-                url, options = (cloudinary_url(
-                    upload_result['public_id'],
-                    format=upload_result['format'],
-                    width=200,
-                    height=150,
-                    crop="fill"
-                )
-                )
-                url = upload_result['secure_url']
-                response = requests.get(url)
-                image = Image.open(io.BytesIO(response.content))
-                image.save("image_001.jpeg")
-                src = "image_001.jpeg"
-                mode = request.form["mode"]
-        except:
-            return render_template("error.html")
+        image = cv2.imread(src)
+        if mode in ['separate_background', 'instance_segmentation']:
+            cfg = detectron.setup_cfg(config_file=model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"),
+                                      weights_file=model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"),
+                                      confidence_threshold=CONFIDENCE)
+            predictor = DefaultPredictor(cfg)
+            outputs = predictor(image)
+            if mode == 'instance_segmentation':
+                metadata_name = cfg.DATASETS.TEST[0] if len(
+                    cfg.DATASETS.TEST) else "__unused"
+                metadata_name = MetadataCatalog.get(metadata_name)
+                visualizer = Visualizer(image[..., ::-1], metadata_name)
+                vis_image = visualizer.draw_instance_predictions(
+                    outputs["instances"].to("cpu"))
+                vis_image = Image.fromarray(
+                    np.uint8(vis_image.get_image()[:, :, ::-1]))
 
-    image = cv2.imread(src)
-    cfg = detectron.setup_cfg(config_file=model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"),
-                              weights_file=model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"),
-                              confidence_threshold=CONFIDENCE)
-    predictor = DefaultPredictor(cfg)
-    outputs = predictor(image)
-    if mode == 'instance_segmentation':
-        metadata_name = cfg.DATASETS.TEST[0] if len(
-            cfg.DATASETS.TEST) else "__unused"
-        metadata_name = MetadataCatalog.get(metadata_name)
-        visualizer = Visualizer(image[..., ::-1], metadata_name)
-        vis_image = visualizer.draw_instance_predictions(
-            outputs["instances"].to("cpu"))
-        vis_image = Image.fromarray(
-            np.uint8(vis_image.get_image()[:, :, ::-1]))
+            else:
+                me_kernel = (7, 7)
+                bg_kernel = (21, 21)
+                desaturate = True
+                instances = outputs["instances"]
+                if not instances.has("pred_masks"):
+                    return
+                mask = instances.pred_masks.cpu().sum(0) >= 1
+                mask = mask.numpy().astype("uint8")*255
+                mask = np.stack([mask, mask, mask], axis=2)
+                mask = cv2.GaussianBlur(mask, me_kernel, 0)
+                foreground = image
+                background = cv2.GaussianBlur(foreground, bg_kernel, 0)
 
-    else:
-        me_kernel = (7, 7)
-        bg_kernel = (21, 21)
-        desaturate = True
-        instances = outputs["instances"]
-        if not instances.has("pred_masks"):
-            return
-        mask = instances.pred_masks.cpu().sum(0) >= 1
-        mask = mask.numpy().astype("uint8")*255
-        mask = np.stack([mask, mask, mask], axis=2)
-        mask = cv2.GaussianBlur(mask, me_kernel, 0)
-        foreground = image
-        background = cv2.GaussianBlur(foreground, bg_kernel, 0)
+                if desaturate:
 
-        if desaturate:
+                    background = cv2.cvtColor(background, cv2.COLOR_BGR2GRAY)
+                    background = cv2.cvtColor(background, cv2.COLOR_GRAY2RGB)
 
-            background = cv2.cvtColor(background, cv2.COLOR_BGR2GRAY)
-            background = cv2.cvtColor(background, cv2.COLOR_GRAY2RGB)
+                foreground = foreground.astype(float)
+                background = background.astype(float)
+                mask = mask.astype(float)/255.0
+                foreground = cv2.multiply(foreground, mask)
+                background = cv2.multiply(background, 1.0 - mask)
+                vis_image = cv2.add(foreground, background)
+                vis_image = Image.fromarray(np.uint8(vis_image[:, :, ::-1]))
 
-        foreground = foreground.astype(float)
-        background = background.astype(float)
-        mask = mask.astype(float)/255.0
-        foreground = cv2.multiply(foreground, mask)
-        background = cv2.multiply(background, 1.0 - mask)
-        vis_image = cv2.add(foreground, background)
-        vis_image = Image.fromarray(np.uint8(vis_image[:, :, ::-1]))
+        elif mode == 'grayscale':
+            vis_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            vis_image = Image.fromarray(np.uint8(vis_image))
 
-    file_object = io.BytesIO()
-    vis_image.save(file_object, 'JPEG')
-    file_object.seek(0)
-    os.remove(src)
-    return send_file(file_object, mimetype='image/jpeg')
+        elif mode == 'blur':
+            vis_image = cv2.medianBlur(image, 5)
+            vis_image = Image.fromarray(np.uint8(vis_image[:, :, ::-1]))
+
+        file_object = io.BytesIO()
+        vis_image.save(file_object, 'JPEG')
+        file_object.seek(0)
+        os.remove(src)
+        return send_file(file_object, mimetype='image/jpeg')
+    except:
+        return render_template("error.html")
 
 
 if __name__ == "__main__":
--- app_local.py
+++ app_local.py
@@ -24,6 +24,19 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+def resizing(img, new_width=None, new_height=None, interp=cv2.INTER_LINEAR):
+    height, width = img.shape[:2]
+    if new_width is None and new_height is None:
+        return img
+    if new_width is None:
+        ratio = new_height / height
+        dimension = (int(width * ratio), new_height)
+    else:
+        ratio = new_width / width
+        dimension = (new_width, int(height * ratio))
+    res_img = cv2.resize(img, dimension, interpolation=interp)
+    return res_img
+
 @app.route("/")
 def home():
     return render_template('home.html')
@@ -31,74 +44,89 @@ def home():
 
 @app.route("/predict", methods=['POST', 'GET'])
 def main():
+    try: 
+        
 
-    CONFIDENCE = .7
+        method = request.method
+        if method == 'GET':
+            url = request.args.get("url")
+            response = requests.get(url)
+            image = Image.open(io.BytesIO(response.content))
+            image.save(os.path.join(app.config['UPLOAD_FOLDER'], "image_001.jpeg"))
+            src = os.path.join(app.config['UPLOAD_FOLDER'], "image_001.jpeg")
+            mode = "instance_segmentation"
+            CONFIDENCE = .7
+        elif method == 'POST':
+            try:
+                file = request.files['file']
 
-    method = request.method
-    if method == 'GET':
-        url = request.args.get("url")
-        response = requests.get(url)
-        image = Image.open(io.BytesIO(response.content))
-        image.save(os.path.join(app.config['UPLOAD_FOLDER'], "image_001.jpeg"))
-        src = os.path.join(app.config['UPLOAD_FOLDER'], "image_001.jpeg")
-        mode = "instance_segmentation"
-    elif method == 'POST':
-        try:
-            file = request.files['file']
-            if file and allowed_file(file.filename):
-                filename = file.filename
-                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-                src = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-                mode = request.form["mode"]
-        except:
-            return render_template("error.html")
+                if file and allowed_file(file.filename):
+                    filename = file.filename
+                    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                    src = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+                    mode = request.form["mode"]
+                    CONFIDENCE = float(request.form["confidence"])
 
-    image = cv2.imread(src)
-    cfg = detectron.setup_cfg(config_file="./configs/COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml",
-                              weights_file=None,
-                              confidence_threshold=CONFIDENCE)
-    predictor = DefaultPredictor(cfg)
-    outputs = predictor(image)
-    if mode == 'instance_segmentation':
-        metadata_name = cfg.DATASETS.TEST[0] if len(cfg.DATASETS.TEST) else "__unused"
-        metadata_name = MetadataCatalog.get(metadata_name)
-        visualizer = Visualizer(image[...,::-1], metadata_name)
-        vis_image = visualizer.draw_instance_predictions(outputs["instances"].to("cpu"))
-        vis_image = Image.fromarray(np.uint8(vis_image.get_image()[:, :, ::-1]))
+            except:
+                return render_template("error.html")
 
-    else:
-        me_kernel=(7, 7)
-        bg_kernel=(21, 21)
-        desaturate=True
-        instances = outputs["instances"]
-        if not instances.has("pred_masks"):
-            return
-        mask = instances.pred_masks.cpu().sum(0) >= 1
-        mask = mask.numpy().astype("uint8")*255
-        mask = np.stack([mask, mask, mask], axis=2)
-        mask = cv2.GaussianBlur(mask, me_kernel, 0)
-        foreground = image
-        background = cv2.GaussianBlur(foreground, bg_kernel, 0)
+        image = cv2.imread(src)
+        if mode in ['separate_background', 'instance_segmentation']:
+            cfg = detectron.setup_cfg(config_file="./configs/COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml",
+                                      weights_file=None,
+                                      confidence_threshold=CONFIDENCE)
+            predictor = DefaultPredictor(cfg)
+            outputs = predictor(image)
+            if mode == 'instance_segmentation':
+                metadata_name = cfg.DATASETS.TEST[0] if len(cfg.DATASETS.TEST) else "__unused"
+                metadata_name = MetadataCatalog.get(metadata_name)
+                visualizer = Visualizer(image[...,::-1], metadata_name)
+                vis_image = visualizer.draw_instance_predictions(outputs["instances"].to("cpu"))
+                vis_image = Image.fromarray(np.uint8(vis_image.get_image()))
 
-        if desaturate:
+            else:
+                me_kernel=(7, 7)
+                bg_kernel=(21, 21)
+                desaturate=True
+                instances = outputs["instances"]
 
-            background = cv2.cvtColor(background, cv2.COLOR_BGR2GRAY)
-            background = cv2.cvtColor(background, cv2.COLOR_GRAY2RGB)
+                if not instances.has("pred_masks"):
+                    return
+                mask = instances.pred_masks.cpu().sum(0) >= 1
+                mask = mask.numpy().astype("uint8")*255
+                mask = np.stack([mask, mask, mask], axis=2)
+                mask = cv2.GaussianBlur(mask, me_kernel, 0)
+                foreground = image
+                background = cv2.GaussianBlur(foreground, bg_kernel, 0)
 
-        foreground = foreground.astype(float)
-        background = background.astype(float)
-        mask = mask.astype(float)/255.0
-        foreground = cv2.multiply(foreground, mask)
-        background = cv2.multiply(background, 1.0 - mask)
-        vis_image = cv2.add(foreground, background)
-        vis_image = Image.fromarray(np.uint8(vis_image[:, :, ::-1]))
+                if desaturate:
 
+                    background = cv2.cvtColor(background, cv2.COLOR_BGR2GRAY)
+                    background = cv2.cvtColor(background, cv2.COLOR_GRAY2RGB)
 
-    file_object = io.BytesIO()
-    vis_image.save(file_object, 'JPEG')
-    file_object.seek(0)
-    os.remove(src)
-    return send_file(file_object, mimetype='image/jpeg')
+                foreground = foreground.astype(float)
+                background = background.astype(float)
+                mask = mask.astype(float)/255.0
+                foreground = cv2.multiply(foreground, mask)
+                background = cv2.multiply(background, 1.0 - mask)
+                vis_image = cv2.add(foreground, background)
+                vis_image = Image.fromarray(np.uint8(vis_image[:, :, ::-1]))
+
+        elif mode == 'grayscale':
+            vis_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            vis_image = Image.fromarray(np.uint8(vis_image))
+
+        elif mode == 'blur':
+            vis_image = cv2.medianBlur(image, 5)
+            vis_image = Image.fromarray(np.uint8(vis_image[:, :, ::-1]))
+
+        file_object = io.BytesIO()
+        vis_image.save(file_object, 'JPEG')
+        file_object.seek(0)
+        os.remove(src)
+        return send_file(file_object, mimetype='image/jpeg')
+    except:
+        return render_template("error.html")
 
 if __name__ == "__main__":
 
